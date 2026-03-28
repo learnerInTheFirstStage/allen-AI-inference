765fd72f90380dd077c1acc3b760c22e2973ee39
learnerInTheFirstStage <allenguo166@gmail.com>
2026-03-28 16:54:05 -0700

    Introduce Shared memory to improve efficiency

--- src/MyTensor.cu
+++ src/MyTensor.cu
@@ -2,6 +2,45 @@
 #include <iostream>
 #include <chrono>
 
+#define TILE_SIZE 32
+
+__global__ void matmul_tiled_kernel(const float* A, const float* B, float* C, int M, int K, int N)
+{
+    // Get shared memory (for each Block)
+    __shared__ float s_A[TILE_SIZE][TILE_SIZE];
+    __shared__ float s_B[TILE_SIZE][TILE_SIZE];
+
+    int row = blockIdx.y * TILE_SIZE + threadIdx.y;
+    int col = blockIdx.x * TILE_SIZE + threadIdx.x;
+    float sum = 0.0f;
+
+    for (int t = 0; t < (K + TILE_SIZE - 1) / TILE_SIZE; ++ t)
+    {
+        // 1. Move the data from VRAM to SHARED MEMORY
+        if (row < M && (t * TILE_SIZE + threadIdx.x) < K)
+            s_A[threadIdx.y][threadIdx.x] = A[row * K + t * TILE_SIZE + threadIdx.x];
+        else
+            s_A[threadIdx.y][threadIdx.x] = 0.0f;
+        
+        if (col < N && (t * TILE_SIZE + threadIdx.y) < K)
+            s_B[threadIdx.y][threadIdx.x] = B[(t * TILE_SIZE + threadIdx.y) * N + col];
+        else
+            s_B[threadIdx.y][threadIdx.x] = 0.0f;
+
+        __syncthreads();
+
+        // 2. Make calculation in SHARED MEMORY
+        for (int k = 0; k < TILE_SIZE; ++ k)
+        {
+            sum += s_A[threadIdx.y][k] * s_B[k][threadIdx.x];
+        }
+
+        __syncthreads();
+
+        if (row < M && col < N) C[row * N + col] = sum;
+    }
+}
+
 /**
  * @brief Simple Matrix Multiplication Kernel
  * Each thread computes one element of the output matrix C.
@@ -40,7 +79,8 @@ extern "C" void launch_matmul_kernel(const float* h_A, const float* h_B, float* 
     cudaMemcpy(d_B, h_B, size_B, cudaMemcpyHostToDevice);
 
     // 3. Define Grid and Block dimensions
-    dim3 threadsPerBlock(16, 16);
+    // dim3 threadsPerBlock(16, 16);
+    dim3 threadsPerBlock(TILE_SIZE, TILE_SIZE);
     dim3 numBlocks((N + threadsPerBlock.x - 1) / threadsPerBlock.x, 
                    (M + threadsPerBlock.y - 1) / threadsPerBlock.y);
     
@@ -48,7 +88,7 @@ extern "C" void launch_matmul_kernel(const float* h_A, const float* h_B, float* 
     auto start = std::chrono::high_resolution_clock::now();
 
     // 4. Launch Kernel
-    matmul_simple_kernel <<< numBlocks, threadsPerBlock >>>  (d_A, d_B, d_C, M, K, N);
+    matmul_tiled_kernel <<< numBlocks, threadsPerBlock >>>  (d_A, d_B, d_C, M, K, N);
 
     cudaDeviceSynchronize();
     auto end = std::chrono::high_resolution_clock::now();
